d205bc49f579b23c3d192368de3142df97e267ca
iridon0920 <iridon0920@gmail.com>
2021-05-06 22:55:53 +0900

    [移動] ゲームオーバー時のテキスト表示、入力コンポーネント生成処理を専用のViewへ

--- Assets/Scripts/GameObject/Game.cs
+++ Assets/Scripts/GameObject/Game.cs
@@ -2,33 +2,14 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using Zenject;
-using UniRx;
 public class Game : MonoBehaviour
 {
     [Inject]
     BlockController.Factory BlockControllerFactory;
     List<BlockController> BlockControllers = new List<BlockController>();
 
-    [SerializeField]
-    private Canvas GameOverText;
-    [SerializeField]
-    private GameObject GameOverController;
-    [Inject]
-    private readonly GameOverEvent GameOverEvent;
     void Awake()
     {
         BlockControllers.Add(BlockControllerFactory.Create());
-
-        GameOverEvent
-            .GameOverObservable
-            .Subscribe(isGameOver =>
-                {
-                    if (isGameOver)
-                    {
-                        GameOverText.enabled = true;
-                        Instantiate(GameOverController);
-                    }
-                }
-            );
     }
 }
--- Assets/Scripts/View/GameOverView.cs
+++ Assets/Scripts/View/GameOverView.cs
@@ -1,5 +1,29 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
+using UniRx;
+
+
 public class GameOverView : MonoBehaviour
 {
+    [SerializeField]
+    private Canvas GameOverText;
+    [SerializeField]
+    private GameObject GameOverController;
+    [Inject]
+    private readonly GameOverEvent GameOverEvent;
+
+    void Awake()
+    {
+        GameOverEvent
+            .GameOverObservable
+            .Subscribe(isGameOver =>
+            {
+                if (isGameOver)
+                {
+                    GameOverText.enabled = true;
+                    Instantiate(GameOverController);
+                }
+            });
+    }
 }
