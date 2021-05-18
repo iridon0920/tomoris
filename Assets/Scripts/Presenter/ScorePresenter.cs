290d7cedacff207dc3b17b5fde094d5dbaf1c3a5
iridon0920 <iridon0920@gmail.com>
2021-05-18 23:03:43 +0900

    [変更] プレイヤー別のスコア位置調整

--- Assets/Scripts/Presenter/ScorePresenter.cs
+++ Assets/Scripts/Presenter/ScorePresenter.cs
@@ -5,6 +5,9 @@ using Zenject;
 
 public class ScorePresenter
 {
+    const float PLAYER_1_POSITION_X = -124f;
+    const float PLAYER_2_POSITION_X = 180f;
+    const float COMMON_POSITION_Y = -225f;
     private readonly ScoreView ScoreView;
 
     [Inject]
@@ -22,11 +25,11 @@ public class ScorePresenter
     {
         if (playerId == 1)
         {
-            ScoreView.ChangePointsPosition(-124, 225);
+            ScoreView.ChangePointsPosition(PLAYER_1_POSITION_X, COMMON_POSITION_Y);
         }
         else if (playerId == 2)
         {
-            ScoreView.ChangePointsPosition(180, 225);
+            ScoreView.ChangePointsPosition(PLAYER_2_POSITION_X, COMMON_POSITION_Y);
         }
     }
 }
--- Assets/Scripts/View/ScoreView.cs
+++ Assets/Scripts/View/ScoreView.cs
@@ -6,7 +6,7 @@ public class ScoreView : MonoBehaviour
     [SerializeField]
     private Text Points;
 
-    public void ChangePointsPosition(int x, int y)
+    public void ChangePointsPosition(float x, float y)
     {
         Points.rectTransform.anchoredPosition = new Vector3(x, y, 0);
     }
